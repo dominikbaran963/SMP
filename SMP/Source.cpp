23830f77dcf571838e4cb0e56b0ff65ca4ed6d10
MaciejBuczek <maciejbuczek98@gmail.com>
2019-06-05 12:29:50 +0200

    input

--- SMP/Source.cpp
+++ SMP/Source.cpp
@@ -33,33 +33,46 @@ struct element {
 
 	} // drugi konstruktor przyjmuje symbol i ilosc preferencji a potem je losowe generuje
 	void printPreferencies() {
-		cout << symbol << ": ";
-		for (int i = 0; i < preferencies.size(); i++)
-			cout << preferencies[i]<<", ";
+		cout << symbol << ":  ";
+		for (int i = 0; i < preferencies.size(); i++) {
+			cout << preferencies[i];
+			if (i != preferencies.size() - 1)
+				cout << ", ";
+		}
 		cout << endl;
 	}
 };
-
-void smp() {
-	vector<element*>set1;
-	vector<element*>set2;
-	int amount1, amount2;
+void getInput(vector<element*>& set1, vector<element*>& set2) {
+	int amount1, amount2, preferenciesSize;
 	char symbol;
 	cout << "ilosc elementow 1: ";
 	cin >> amount1;
 	cout << "ilosc elementow 2: ";
 	cin >> amount2;
+	if (amount1 == amount2)
+		preferenciesSize = amount1;
+	else if (amount1 > amount2)
+		preferenciesSize = amount1;
+	else
+		preferenciesSize = amount2;
 	for (int i = 0; i < amount1; i++) {
-		set1.push_back(new element(static_cast<char>(65 + i), amount1));
+		set1.push_back(new element(static_cast<char>(65 + i), preferenciesSize));
 	}
 	for (int i = 0; i < amount2; i++) {
-		set2.push_back(new element(static_cast<char>(48 + i), amount2 + 2));
+		set2.push_back(new element(static_cast<char>(48 + i), preferenciesSize));
 	}
-	for (int i = 0; i < set1.size(); i++)
+	for (int i = 0; i < set1.size(); i++) {
 		set1[i]->printPreferencies();
+	}
 	cout << endl;
-	for (int i = 0; i < set2.size(); i++)
+	for (int i = 0; i < set2.size(); i++) {
 		set2[i]->printPreferencies();
+	}
+}
+void smp() {
+	vector<element*>set1;
+	vector<element*>set2;
+	getInput(set1, set2);
 }
 
 int main() {
